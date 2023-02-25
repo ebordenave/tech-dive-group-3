f7a471bc243cc365997c0a2169a99a651f07e39c
Abu <abujames2314@gmail.com>
2023-02-25 16:43:05 -0500

    change the table exams table using arrays

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+// var name router 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('API is working properly!');
--- client/src/components/ExamsTable.jsx
+++ client/src/components/ExamsTable.jsx
@@ -1,60 +1,56 @@
-import React from 'react';
-import './ExamsTable.css';
-import ExamTableRow from './ExamTableRow.jsx';
+import React, { useState, useEffect } from "react";
+import "./ExamsTable.css";
+import ExamTableRow from "./ExamTableRow";
 
-export default class ExamsTable extends React.Component {
-    state = { exams: null }
-  
-    componentDidMount() {
-      fetch('https://czi-covid-lypkrzry4q-uc.a.run.app/api/exams')
-        .then(response => response.json())
-        .then(data => { this.setState({exams: data.exams}) });
-    }
-  
-    render() {
-      const { exams } = this.state;
-      const headings = [
-        "Patient ID", 
-        "Exam ID", 
-        "Image", 
-        "Key Findings", 
-        "Brixia Scores", 
-        "Age", 
-        "Sex", 
-        "BMI", 
-        "Zip" 
-      ];
+const ExamsTable = () => {
+  const [exams, setExams] = useState(null);
 
-      const isLoading = exams === null;
-      return (
-        <div>
-          <div>
-            <div>
-              <table className="uk-table">
-                <thead>
-                  <tr>
-                    { isLoading 
-                      ? <p></p>
-                      :
-                      headings.map((h, index) => <th>{h}</th>)
-                    }
-                  </tr>
-                </thead>
-                <tbody>
-                  {isLoading
-                    ? <h1> Loading </h1>
-                    : exams.map((exam, index) =>
-                        <ExamTableRow key={index} index={index + 1} exam={exam}/>
-                      )
-                  }
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      );
-    }
-  }
-  
+  useEffect(() => {
+    fetch("https://czi-covid-lypkrzry4q-uc.a.run.app/api/exams")
+      .then((response) => response.json())
+      .then((data) => setExams(data.exams));
+  }, []);
 
-  // Derived from https://codesandbox.io/s/collapsible-table-rows-in-react-ybb28?file=/index.js
+  const headings = [
+    "Patient ID",
+    "Exam ID",
+    "Image",
+    "Key Findings",
+    "Brixia Scores",
+    "Age",
+    "Sex",
+    "BMI",
+    "Zip",
+  ];
+
+  const isLoading = exams === null;
+
+  return (
+    <div className="table-container">
+      <table className="exams-table">
+        <thead>
+          <tr>
+            {isLoading ? (
+              <th>Loading</th>
+            ) : (
+              headings.map((h, index) => <th key={index}>{h}</th>)
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading ? (
+            <tr>
+              <td>Loading</td>
+            </tr>
+          ) : (
+            exams.map((exam, index) => (
+              <ExamTableRow key={index} index={index + 1} exam={exam} />
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ExamsTable;
