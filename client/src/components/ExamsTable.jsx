96509599745824cc5f85d6bc189fee230b1241bd
Dominic Salinas <domsal2002@gmail.com>
2023-03-05 21:32:39 -0500

    remove dependability on modal and add create

--- client/src/components/ExamsTable.jsx
+++ client/src/components/ExamsTable.jsx
@@ -1,56 +1,83 @@
-import React, { useState, useEffect } from "react";
-import "./ExamsTable.css";
-import ExamTableRow from "./ExamTableRow";
+import React from 'react';
+import './ExamsTable.css';
+import ExamTableRow from './ExamTableRow.jsx';
 
-const ExamsTable = () => {
-  const [exams, setExams] = useState(null);
+export default class ExamsTable extends React.Component {
+  // Initialize state with exams and error
+  state = {
+    exams: null,
+    error: null,
+  };
+  
+  componentDidMount() {
+    // Fetch data from the server and set state with response data
+    fetch('http://localhost:9000/exams/')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ exams: data.exams });
+      })
+      .catch(error => {
+        console.error('Error fetching exams:', error);
+      });
+  }
+  
+  render() {
+    const { exams, error } = this.state;
 
-  useEffect(() => {
-    fetch("https://czi-covid-lypkrzry4q-uc.a.run.app/api/exams")
-      .then((response) => response.json())
-      .then((data) => setExams(data.exams));
-  }, []);
+    // Define table headings
+    const headings = [
+      "Patient ID", 
+      "Exam ID", 
+      "Image", 
+      "Key Findings", 
+      "Brixia Scores", 
+      "Age", 
+      "Sex", 
+      "BMI", 
+      "Zip" 
+    ];
 
-  const headings = [
-    "Patient ID",
-    "Exam ID",
-    "Image",
-    "Key Findings",
-    "Brixia Scores",
-    "Age",
-    "Sex",
-    "BMI",
-    "Zip",
-  ];
+    // If there's an error, display an error message
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
 
-  const isLoading = exams === null;
+    // If exams are not loaded yet, display a loading message
+    const isLoading = exams === null;
+    return (
+      <div>
+        <div>
+          <div>
+            <table className="uk-table">
+              <thead>
+                <tr>
+                  {isLoading ? (
+                    <th>Loading...</th>
+                  ) : (
+                    // Render table headings
+                    headings.map((h, index) => <th key={index}>{h}</th>)
+                  )}
+                </tr>
+              </thead>
+              <tbody>
+                {isLoading ? (
+                  <tr>
+                    <td>Loading...</td>
+                  </tr>
+                ) : (
+                  // Render table rows for each exam
+                  exams.map((exam, index) => (
+                    <ExamTableRow key={index} index={index + 1} exam={exam} />
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-  return (
-    <div className="table-container">
-      <table className="exams-table">
-        <thead>
-          <tr>
-            {isLoading ? (
-              <th>Loading</th>
-            ) : (
-              headings.map((h, index) => <th key={index}>{h}</th>)
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
-            <tr>
-              <td>Loading</td>
-            </tr>
-          ) : (
-            exams.map((exam, index) => (
-              <ExamTableRow key={index} index={index + 1} exam={exam} />
-            ))
-          )}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default ExamsTable;
+// Derived from https://codesandbox.io/s/collapsible-table-rows-in-react-ybb28?file=/index.js
--- client/src/hooks/use-api.js
+++ client/src/hooks/use-api.js
@@ -1,17 +1,28 @@
 import { useState, useEffect } from 'react';
 
+// Define API root URL
 const API_ROOT = 'http://localhost:9000';
 
+// Custom hook to make API requests and return response, isLoading and error states
 export function useApi({ path } = { path: '' }) {
+  // Initialize state hooks with default values
   const [response, setResponse] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    // Make API request and update state based on response
     fetch(`${API_ROOT}/${path}`)
-      .then(res => res.text())
-      .then(res => setResponse(res));
+      .then(res => res.json()) // convert response to JSON
+      .then(res => setResponse(res)) // set response state
+      .catch(error => setError(error)) // set error state
+      .finally(() => setIsLoading(false)); // set loading state to false
   }, []);
 
+  // Return an object containing response, loading and error states
   return {
-    response
+    response,
+    isLoading,
+    error
   };
 }
