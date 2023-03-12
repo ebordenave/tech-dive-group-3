b0b7b8bc65f093edebc590f1faf485415eb61c05
Edward Bordenave <edward.bordenave@gmail.com>
2023-03-11 21:41:35 -0500

    changed CORS in app.js

--- api/app.js
+++ api/app.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Use middleware to set up logging, CORS, JSON and URL encoded request bodies, and cookie parsing
 app.use(logger('dev'));
-app.use(cors({origin:["http://localhost:9000","https://group-three-tech-dive.onrender.com"]}));
+app.use(cors({origin:["https://group-three-tech-dive-api.onrender.com","https://group-three-tech-dive.onrender.com"]}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
